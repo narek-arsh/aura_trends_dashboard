a0418215db72897f02c1bc4beb797ffd203fac4d
narek-arsh <narekikol@gmail.com>
2025-09-01 00:42:33 +0200

    Update weather.py

--- app/components/weather.py
+++ app/components/weather.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def render_weather():
+    st.markdown("""
+<iframe src="https://narek-arsh.github.io/malaga-weather-widget/" width="100%" height="80" frameborder="0" scrolling="no"></iframe>
+""", unsafe_allow_html=True)
 # Widget del clima
