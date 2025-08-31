fc3e26e083ebf30aecfef4e6f356a4c3b9d16f1c
narek-arsh <narekikol@gmail.com>
2025-08-31 21:21:27 +0200

    Delete ai_utils.py

--- ai_utils.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-import os
-
-def configure_gemini():
-    api_key = os.getenv("GEMINI_API_KEY")
-    genai.configure(api_key=api_key)
-    return genai.GenerativeModel("gemini-1.5-flash-latest")
